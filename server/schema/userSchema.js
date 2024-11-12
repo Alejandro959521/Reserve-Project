c9d013aee9d6e1b4c49853bed76c77a371855257
Miguel Ojeda <cabrerasm081@gmail.com>
2024-11-11 21:51:46 -0400

    .

--- server/schema/userSchema.js
+++ server/schema/userSchema.js
@@ -5,19 +5,22 @@ const email = Joi.string().email();
 const password = Joi.string().min(5);
 const name = Joi.string().min(2).max(50);
 const phone =Joi.string().pattern(/^[0-9\-+() ]+$/).min(7).max(15);
+const role = Joi.string();
 
 const createUserSchema = Joi.object({
   email: email.required(),
   password: password.required(),
   name: name.required(),
-  phone: phone.required()
+  phone: phone.required(),
+  role: role.required()
 });
 
 const updateUserSchema = Joi.object({
   email: email,
   password: password,
   name: name,
-  phone: phone
+  phone: phone,
+  role: role
 
 });
 
